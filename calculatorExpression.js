16512221900ceed0f345f6ff3969a4e89b5de117
Gustavo Mayorga <redvioletoficial@gmail.com>
2024-02-11 01:02:40 -0600

    save

---
+++ calculatorExpression.js
@@ -0,0 +1,141 @@
+class CalculatorExpression {
+
+    operatorsArr = [];
+    /**
+     * 
+     * @param {string} expresion //expresion usada para parsear la informacion
+     */
+    constructor(expresion) {
+        this.expresion = expresion.replaceAll(" ", "");
+        this.separarDelimitadores();
+    }
+
+
+    /**
+     * Funcion para separar la expresion por operadores y numeros y crear la materia prima
+     */
+    separarDelimitadores() {
+        const expresionArr = this.expresion.split("");
+
+        const delimitadores = /[()\+\-\*\/&\^]/;
+
+        const numerosArr = this.expresion.split(delimitadores).filter((char => {
+            if (char.length >= 1) {
+                return char
+            }
+        }));
+
+        const operadoresArr = [];
+        const operadoresSinParentesis = [];
+
+        expresionArr.forEach((char) => {
+            if (char === "(" || char === ")" || char === "+" || char === "-" || char === "*" || char === "/" || char === "%" || char === "^") {
+                operadoresArr.push(char);
+            }
+        })
+
+        expresionArr.forEach((char) => {
+            if (char === "+" || char === "-" || char === "*" || char === "/" || char === "%" || char === "^") {
+                operadoresSinParentesis.push(char);
+            }
+        })
+
+        //Verificar si hay un error
+        if (operadoresSinParentesis.length == numerosArr.length - 1) {
+            //Materia prima 
+            this.escaneoJerarquia(operadoresArr, numerosArr)
+        } else {
+            console.log("Hay un error en esta madre");
+        }
+    }
+
+
+    /**
+     * Funcion que reliza las operaciones por orden de jearquia
+     * @param {string[]} operadores 
+     * @param {number[]} numeros 
+     */
+    escaneoJerarquia(operadores, numeros) {
+        let cola_operadores = []; //los operadores que van a ser usados
+        let parentesis_inicio = 0;
+        let parentesis_fin = 0;
+        let estaCerrado = false;
+        let idx = 0;
+        let size = operadores.length;
+        let recorrer = 0;
+
+        while (!estaCerrado) {
+            const operador = operadores[idx];
+            if (operador == "(") {
+                cola_operadores = [];
+                operadores.forEach((operador, i) => {
+                    if (operador.match(/[\+\-\*\/&\^]/) && i < idx) {
+                        recorrer++;
+                    }
+                })
+                parentesis_inicio = idx;
+            } else if (operador == ")") {
+                estaCerrado = true;
+                parentesis_fin = idx;
+            } else {
+                cola_operadores.push(operador);
+            }
+            if (idx == (size - 1)) {
+                estaCerrado = true;
+            }
+            idx++;
+        }
+        let x;
+        let exit = true;
+        while (exit) {
+            if (cola_operadores.includes("^")) {
+                const index_operador = cola_operadores.indexOf("^");
+                cola_operadores.splice(index_operador, 1);
+                const p1 = parseFloat(numeros.splice(index_operador + recorrer, 1)[0]);
+                const p2 = parseFloat(numeros.splice(index_operador + recorrer, 1)[0]);
+                const resultado = p1 ** p2;
+                numeros.splice(index_operador + recorrer, 0, resultado);
+            } else if (cola_operadores.includes("*")) {
+                const index_operador = cola_operadores.indexOf("*");
+                cola_operadores.splice(index_operador, 1);
+                const p1 = parseFloat(numeros.splice(index_operador + recorrer, 1)[0]);
+                const p2 = parseFloat(numeros.splice(index_operador + recorrer, 1)[0]);
+                const resultado = p1 * p2;
+                numeros.splice(index_operador + recorrer, 0, resultado);
+            } else if (cola_operadores.includes("/")) {
+                const index_operador = cola_operadores.indexOf("/");
+                cola_operadores.splice(index_operador, 1);
+                const p1 = parseFloat(numeros.splice(index_operador + recorrer, 1)[0]);
+                const p2 = parseFloat(numeros.splice(index_operador + recorrer, 1)[0]);
+                const resultado = p1 / p2;
+                numeros.splice(index_operador + recorrer, 0, resultado);
+            } else if (cola_operadores.includes("+")) {
+                const index_operador = cola_operadores.indexOf("+");
+                cola_operadores.splice(index_operador, 1);
+                const p1 = parseFloat(numeros.splice(index_operador + recorrer, 1)[0]);
+                const p2 = parseFloat(numeros.splice(index_operador + recorrer, 1)[0]);
+                const resultado = p1 + p2;
+                numeros.splice(index_operador + recorrer, 0, resultado);
+            } else if (cola_operadores.includes("-")) {
+                const index_operador = cola_operadores.indexOf("-");
+                cola_operadores.splice(index_operador, 1);
+                const p1 = parseFloat(numeros.splice(index_operador + recorrer, 1)[0]);
+                const p2 = parseFloat(numeros.splice(index_operador + recorrer, 1)[0]);
+                const resultado = p1 - p2;
+                numeros.splice(index_operador + recorrer, 0, resultado);
+            }
+            if (cola_operadores.length == 0) {
+                const elementosSize = (parentesis_fin - parentesis_inicio) + 1;
+                operadores.splice(parentesis_inicio, elementosSize);
+                exit = false;
+                if (operadores.length > 0 && numeros.length > 1) {
+                    this.escaneoJerarquia(operadores, numeros);
+                } else {
+                    console.log("Resultado: ", numeros[0])
+                }
+            }
+        }
+    }
+}
+
+module.exports = CalculatorExpression;
---
+++ index.js
@@ -0,0 +1,20 @@
+const CalculatorExpression = require("./calculatorExpression");
+
+// const formula = "((250-175)-(157-567))";// Resultado 485
+//const formula = "(10+5)*3/2"; // R esultado 22.5
+// const formula = "(10+5)*3/2+2*50"; //Resultado 122.5
+//const formula = "((16+15)*23+5)/2"; //Resultado 359
+//const formula = "((12+8)-(5*3))/2"; // Resultado 2.5
+// const formula = "(30-5)*(6/2)";// Resultado 75
+//const formula = "((40+10)-(8+2))"; // Resultado 40
+//const formula = "((20-8)*(15+5))/4"; //Resultado 60
+//const formula = "((100-25)-(8+2))"; // Resultado: 65
+//const formula= "(15*4)+(3/2)"; // Resultado: 61.5
+//const formula = "5^2"; //25
+//const formula = "(             5 ^ 2 ) ^ 2";//625
+
+
+//formulas malas
+// const formula = "(10^2)5";
+const calculadora = new CalculatorExpression(formula);
+
